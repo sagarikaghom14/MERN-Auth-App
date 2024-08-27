00d30d542da3f31fdb90d6595332859dc04c1a36
sagarikaghom14 <sagarika.ghom@mitaoe.ac.in>
2024-08-27 11:05:28 +0530

    add a middleware and a function to handle errors

--- api/server.js
+++ api/server.js
@@ -21,6 +21,16 @@ app.use(express.json());
 app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
 
+app.use((err,req,res,next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    return res.status(statusCode).json({
+        success: false,
+        message,
+        statusCode,
+    });
+});
+
 app.listen(3000, () => {
     console.log("Server is listening on port 3000");
 });
